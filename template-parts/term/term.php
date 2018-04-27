d9908bdbf05eace2054ec3156d9ded2776cdcd82
metamn <bartus.csongor@gmail.com>
2018-04-27 14:31:54 +0300

    Working on: Connect taxonomies #26

--- template-parts/term/term.php
+++ template-parts/term/term.php
@@ -12,10 +12,12 @@
 <?php
   $term = get_query_var( 'term' );
   if ( ! empty( $term ) ) { ?>
+
     <div class="term">
       <a class="link" href="<?php echo esc_url( get_term_link( $term->term_id ) ) ?>" title="<?php echo $term->name ?>">
         <?php echo $term->name ?>
       </a>
     </div>
+    
   <?php }
 ?>
