188a7b8ee06a89550275d4536343b176adcd5720
metamn <bartus.csongor@gmail.com>
2018-04-30 09:35:17 +0300

    Working on: Create an all inclusive archive page #15

--- page-archives.php
+++ page-archives.php
@@ -10,7 +10,7 @@
 
 <?php get_header() ?>
 
-<section class="content archives">
+<section class="content content-archives">
   <h3 class="archives-title">
     <?php esc_html_e( 'Archives', 'log-lolla-pro' ); ?>
   </h3>
