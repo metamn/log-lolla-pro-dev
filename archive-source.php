bce284dee38a9050fee304b3fd2043422d110363
metamn <bartus.csongor@gmail.com>
2018-05-04 15:24:40 +0300

    Working on archives #15

---
+++ archive-source.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying archive pages for Sources
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Log_Lolla
+ */
+
+get_header(); ?>
+
+	<section class="content content-archive">
+	  <?php get_template_part( 'template-parts/archive/parts/archive', 'title' ); ?>
+    <?php get_template_part( 'template-parts/archive/parts/archive', 'description' ); ?>
+		<?php get_template_part( 'template-parts/archive/archive', '' ); ?>
+	</section>
+
+	<?php get_template_part( 'template-parts/sidebar/sidebar' ); ?>
+
+<?php
+get_footer();
