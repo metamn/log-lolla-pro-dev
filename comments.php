9b0c86f541aaae5268f3658eddb75d1e9933e9d5
metamn <bartus.csongor@gmail.com>
2018-05-05 09:33:56 +0300

    Working on comments #16

--- comments.php
+++ comments.php
@@ -31,13 +31,13 @@ if ( post_password_required() ) {
 			if ( 1 === $comment_count ) {
 				printf(
 					/* translators: 1: title. */
-					esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', 'log-lolla' ),
+					esc_html_e( 'One update on &ldquo;%1$s&rdquo;', 'log-lolla' ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			} else {
 				printf( // WPCS: XSS OK.
 					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'log-lolla' ) ),
+					esc_html( _nx( '%1$s update on &ldquo;%2$s&rdquo;', '%1$s updates on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'log-lolla' ) ),
 					number_format_i18n( $comment_count ),
 					'<span>' . get_the_title() . '</span>'
 				);
@@ -58,12 +58,7 @@ if ( post_password_required() ) {
 
 		<?php the_comments_navigation();
 
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) : ?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'log-lolla' ); ?></p>
-		<?php
-		endif;
-
+		
 	endif; // Check for have_comments().
 
 	comment_form();
