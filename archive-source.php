8a7c9efe25797a58a242c40356101b4e864e27f4
metamn <bartus.csongor@gmail.com>
2018-06-11 09:34:04 +0300

    Refactoring templates

---
+++ archive-source.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template for displaying archive pages for Sources
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Log_Lolla_Pro
+ */
+
+get_header(); ?>
+
+	<section class="content content-archive">
+		<h3 class="hidden">Archive for Sources</h3>
+
+		<?php get_template_part( 'template-parts/archive/archive', 'posts' ); ?>
+		<?php get_template_part( 'template-parts/archive/archive', 'header' ); ?>
+	</section>
+
+	<?php get_template_part( 'template-parts/sidebar/sidebar' ); ?>
+
+<?php
+get_footer();
