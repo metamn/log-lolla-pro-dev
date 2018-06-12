3f64fadb1df8037d7c40a315454e819eea85deeb
metamn <bartus.csongor@gmail.com>
2018-06-12 10:50:14 +0300

    Refactoring to list items

--- template-parts/archive/parts/archive-counter.php
+++ template-parts/archive/parts/archive-counter.php
@@ -9,9 +9,16 @@
    */
 ?>
 
-<div class="archive-counter">
-  <?php get_template_part( 'template-parts/framework/structure/pictogram/pictogram', '' ); ?>
+<?php
+  set_query_var( 'list_item_class', 'archive-counter' );
+  set_query_var( 'list_item_primary_text', $pictogram['text'] );
+  set_query_var( 'list_item_secondary_text', '' );
+  set_query_var( 'list_item_graphic', $pictogram['number'] );
 
-  <?php set_query_var( 'arrow_direction', 'bottom' ); ?>
-  <?php get_template_part( 'template-parts/framework/design/decorations/arrow-with-triangle/arrow-with-triangle', '' ); ?>
-</div>
+  ob_start();
+  set_query_var( 'arrow_direction', 'bottom' );
+  get_template_part( 'template-parts/framework/design/decorations/arrow-with-triangle/arrow-with-triangle', '' );
+  set_query_var( 'list_item_icon', ob_get_clean() );
+
+  get_template_part( 'template-parts/framework/structure/list-item/list-item', '' );
+?>
