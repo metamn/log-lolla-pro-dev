324089864592cd3f8379ec67fbe369755e67b509
metamn <bartus.csongor@gmail.com>
2018-06-15 09:50:38 +0300

    Formatting code

--- 404.php
+++ 404.php
@@ -1,48 +1,45 @@
 <?php
-	/**
-	 * Displays a 404 page (not found)
-	 *
-	 * Displays a page not found message and a search form
-	 *
-	 * @link https://morethemes.baby/log-lolla-pro-pro-demo/peoplexxx Live example
-	 * @link https://codex.wordpress.org/Creating_an_Error_404_Page Wordpress documentation
-	 *
-	 * @package Log_Lolla_Pro
-	 * @since 1.0.0
-	 */
+/**
+ * Displays a 404 page (not found)
+ *
+ * Displays a page not found message and a search form
+ *
+ * @link https://morethemes.baby/log-lolla-pro-pro-demo/peoplexxx Live example
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page Wordpress documentation
+ *
+ * @package Log_Lolla_Pro
+ * @since 1.0.0
+ */
 
-	get_header();
+get_header();
 ?>
 
-	<section class="content content-none">
-		<h3 class="hidden">Page not found</h3>
+<section class="content content-none">
+	<h3 class="hidden">Page not found</h3>
 
-		<article class="post">
-			<h3 class="post-title">
-				<span class="text">
-					<?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'log-lolla-pro' ); ?>
-				</span>
-			</h3>
+	<article class="post">
+		<h3 class="post-title">
+			<span class="text">
+				<?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'log-lolla-pro' ); ?>
+			</span>
+		</h3>
 
-			<aside class="post-content">
-				<h3 class="hidden">Article content</h3>
+		<aside class="post-content">
+			<h3 class="hidden">Article content</h3>
 
-				<div class="text">
-					<?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'log-lolla-pro' ); ?>
-				</div>
-			</aside>
+			<div class="text">
+				<?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'log-lolla-pro' ); ?>
+			</div>
+		</aside>
 
-			<aside class="search">
-				<h3 class="hidden">Search form</h3>
+		<aside class="search">
+			<h3 class="hidden">Search form</h3>
 
-				<?php get_search_form(); ?>
-			</aside>
-		</article>
-	</section>
+			<?php get_search_form(); ?>
+		</aside>
+	</article>
+</section>
 
-	<?php
-		get_template_part( 'template-parts/sidebar/sidebar' );
-	?>
+<?php get_template_part( 'template-parts/sidebar/sidebar' ); ?>
 
-<?php
-get_footer();
+<?php get_footer(); ?>
