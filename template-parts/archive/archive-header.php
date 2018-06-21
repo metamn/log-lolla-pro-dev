3f56fa43bd4eda4260f44577b4bf7f4ebafca6e0
metamn <bartus.csongor@gmail.com>
2018-06-21 08:37:53 +0300

    Working on the documentation

--- template-parts/archive/archive-header.php
+++ template-parts/archive/archive-header.php
@@ -1,16 +1,22 @@
 <?php
-  /**
-   * Display archive header
-   *
-   *
-   * @package Log_Lolla_Pro
-   */
+/**
+ * Displays the archive header.
+ *
+ * The archive header contains:
+ *  * A Breadcrumb template part.
+ *  * Archive title and description template part.
+ *  * The Archive counters template part.
+ *
+ * @link https://morethemes.baby/log-lolla-pro-pro-demo/tag/indie-web/ Live example
+ * @package Log_Lolla_Pro
+ * @since 1.0.0
+ */
 
-  $klass = '';
+$klass = '';
 ?>
 
 <header class="archive-header">
-  <?php get_template_part( 'template-parts/breadcrumb/breadcrumb', 'archives' ); ?>
-  <?php get_template_part( 'template-parts/archive/parts/archive', 'title-and-description' ); ?>
-  <?php get_template_part( 'template-parts/archive/parts/archive', 'counters' ); ?>
+	<?php get_template_part( 'template-parts/breadcrumb/breadcrumb', 'archives' ); ?>
+	<?php get_template_part( 'template-parts/archive/parts/archive', 'title-and-description' ); ?>
+	<?php get_template_part( 'template-parts/archive/parts/archive', 'counters' ); ?>
 </header>
