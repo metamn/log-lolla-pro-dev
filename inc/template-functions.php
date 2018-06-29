ccea808afd5207b7531bb51a8b18293a63d1f7fa
metamn <bartus.csongor@gmail.com>
2018-06-29 11:14:00 +0300

    Fixing Continue reading for posts

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -28,6 +28,7 @@ add_filter( 'get_the_archive_title', 'log_lolla_pro_get_the_archive_title', 10, 
 /**
  * Remove `<p>` and `<br>` tags added by WordPress.
  * If not, the 'Continue reading -->' arrow will be completely broken.
+ * For `the_content()` check the `post-content-standard.php`.
  *
  * @link https://wordpress.stackexchange.com/questions/130075/stop-wordpress-automatically-adding-br-tags-to-post-content
  */
