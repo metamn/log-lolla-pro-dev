0cebdbe0b6eacd5f479909564232868af911abe3
metamn <bartus.csongor@gmail.com>
2018-07-25 16:56:52 +0300

    Ready for a final refactoring

--- template-parts/comment/parts/comment-content-or-excerpt.php
+++ template-parts/comment/parts/comment-content-or-excerpt.php
@@ -8,16 +8,8 @@
  * @since 1.0.0
  */
 
-$klass = '';
-?>
-
-<aside class="comment-content-or-excerpt">
-	<h3 class="hidden">Comment content or excerpt</h3>
-	<?php
-	if ( log_lolla_pro_count_words( get_comment_text() ) > 60 ) {
-		get_template_part( 'template-parts/comment/parts/comment', 'excerpt' );
-	} else {
-		get_template_part( 'template-parts/comment/parts/comment', 'content' );
-	}
-	?>
-</aside>
+if ( log_lolla_pro_count_words( get_comment_text() ) > 60 ) {
+	get_template_part( 'template-parts/comment/parts/comment', 'excerpt' );
+} else {
+	get_template_part( 'template-parts/comment/parts/comment', 'content' );
+}
