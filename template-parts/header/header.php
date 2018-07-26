d8930d9a569f38c7876004d16bb6006730f5d67f
metamn <bartus.csongor@gmail.com>
2018-07-26 10:31:09 +0300

    Refactoring classnames

--- template-parts/header/header.php
+++ template-parts/header/header.php
@@ -15,11 +15,11 @@ if ( function_exists( 'log_lolla_pro_get_header_class' ) ) {
 ?>
 
 <header class="header <?php echo esc_attr( $klass ); ?>">
-	<?php get_template_part( 'template-parts/header/header', 'image' ); ?>
-	<?php get_template_part( 'template-parts/header/header', 'logo' ); ?>
-	<?php get_template_part( 'template-parts/header/header', 'title' ); ?>
-	<?php get_template_part( 'template-parts/header/header', 'subtitle' ); ?>
-	<?php get_template_part( 'template-parts/header/header', 'menu-hamburger' ); ?>
+	<?php get_template_part( 'template-parts/header/parts/header', 'image' ); ?>
+	<?php get_template_part( 'template-parts/header/parts/header', 'logo' ); ?>
+	<?php get_template_part( 'template-parts/header/parts/header', 'title' ); ?>
+	<?php get_template_part( 'template-parts/header/parts/header', 'subtitle' ); ?>
+	<?php get_template_part( 'template-parts/header/parts/header', 'menu-hamburger' ); ?>
 </header>
 
-<?php get_template_part( 'template-parts/header/header', 'menu' ); ?>
+<?php get_template_part( 'template-parts/header/parts/header', 'menu' ); ?>
