c5c4758a078f42b7d250bab26973dce485403be4
metamn <bartus.csongor@gmail.com>
2018-07-31 21:19:16 +0300

    Refactoring layout width

--- template-parts/sidebar/sidebar.php
+++ template-parts/sidebar/sidebar.php
@@ -12,11 +12,10 @@ $klass = '';
 <section id="sidebar" class="sidebar">
 	<h3 class="sidebar-title">Sidebar</h3>
 
-	// we need that to size widgets inside
 	<section class="widget-list">
-		<h3 class="widget-list">Widget list</h3>
+		<h3 class="list-title">Widget list</h3>
 
-		<div class="widget-list">
+		<div class="list-items">
 			<?php get_sidebar(); ?>
 		</div>
 	</div>
