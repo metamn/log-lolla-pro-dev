cc4cd2a440e92535a79561ebc1ee8667ec90207a
metamn <bartus.csongor@gmail.com>
2018-07-31 20:46:32 +0300

    Before refactoring post widths

--- template-parts/sidebar/sidebar.php
+++ template-parts/sidebar/sidebar.php
@@ -12,7 +12,12 @@ $klass = '';
 <section id="sidebar" class="sidebar">
 	<h3 class="sidebar-title">Sidebar</h3>
 
-	<div class="sidebar-widget-list">
-		<?php get_sidebar(); ?>
+	// we need that to size widgets inside
+	<section class="widget-list">
+		<h3 class="widget-list">Widget list</h3>
+
+		<div class="widget-list">
+			<?php get_sidebar(); ?>
+		</div>
 	</div>
 </section>
